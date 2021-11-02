766413b2e16278f28cb795751b572b254808f674
Denis Mazurchuk <denis.aurorim@gmail.com>
2021-11-02 20:05:25 +0200

    Styling with server-side rendering

--- server-side-rendering/server.js
+++ server-side-rendering/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import React from "react";
 import { renderToString } from "react-dom/server";
+import { ServerStyleSheet } from "styled-components";
 import { StaticRouter } from "react-router-dom";
 import path from "path";
 import fs from "fs";
@@ -11,19 +12,26 @@ const app = express();
 app.use(express.static("./build", { index: false }));
 
 app.get("/*", (req, res) => {
+  const sheet = new ServerStyleSheet();
+
   const reactApp = renderToString(
-    <StaticRouter location={req.url}>
-      <App />
-    </StaticRouter>
+    sheet.collectStyles(
+      <StaticRouter location={req.url}>
+        <App />
+      </StaticRouter>
+    )
   );
 
   const templateFile = path.resolve("./build/index.html");
+
   fs.readFile(templateFile, "utf-8", (err, data) => {
     if (err) {
       return res.status(500).send(err);
     }
     return res.send(
-      data.replace('<div id="root"></div>', `<div id="root">${reactApp}</div>`)
+      data
+        .replace('<div id="root"></div>', `<div id="root">${reactApp}</div>`)
+        .replace(`{{ styles }}`, sheet.getStyleTags)
     );
   });
 });
