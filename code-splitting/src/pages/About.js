906a4cf664cfe3a0a1ad03d1ae4c0adc5daf599e
Denis Mazurchuk <denis.aurorim@gmail.com>
2021-11-03 21:31:45 +0200

    Error boundaries

--- code-splitting/src/pages/About.js
+++ code-splitting/src/pages/About.js
@@ -1,4 +1,5 @@
 import React, { useState, lazy, Suspense } from 'react';
+import ErrorBoundary from '../ErrorBoundary';
 // import { One } from './One';
 // import { Two } from './Two';
 // import { Three } from './Three';
@@ -15,9 +16,15 @@ export const About = () => {
 		<h1>About</h1>
 		{showComponents && (
 			<Suspense fallback={<p>Loading Components...</p>}>
-				<One />
-				<Two />
-				<Three />
+				<ErrorBoundary>
+					<One />
+				</ErrorBoundary>
+				<ErrorBoundary>
+					<Two />
+				</ErrorBoundary>
+				<ErrorBoundary>
+					<Three />
+				</ErrorBoundary>
 			</Suspense>
 		)}
 		<button onClick={() => setShowComponents(true)}>Show</button>
--- code-splitting/src/pages/One.js
+++ code-splitting/src/pages/One.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export const One = () => {
+	throw new Error('Help!');
 	return (
 		<h2>One</h2>
 	);
