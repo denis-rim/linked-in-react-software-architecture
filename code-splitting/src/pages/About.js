85c642cadc4d395b6afe518bdb1107e884b9527b
Denis Mazurchuk <denis.aurorim@gmail.com>
2021-11-03 21:14:32 +0200

    When to use code splitting

--- code-splitting/src/pages/About.js
+++ code-splitting/src/pages/About.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { useState, lazy, Suspense } from 'react';
 // import { One } from './One';
 // import { Two } from './Two';
 // import { Three } from './Three';
@@ -8,14 +8,19 @@ const Two = lazy(() => import('./Two'));
 const Three = lazy(() => import('./Three'));
 
 export const About = () => {
+	const [showComponents, setShowComponents] = useState(false);
+
 	return (
 		<>
 		<h1>About</h1>
-		<Suspense fallback={<p>Loading Components...</p>}>
-			<One />
-			<Two />
-			<Three />
-		</Suspense>
+		{showComponents && (
+			<Suspense fallback={<p>Loading Components...</p>}>
+				<One />
+				<Two />
+				<Three />
+			</Suspense>
+		)}
+		<button onClick={() => setShowComponents(true)}>Show</button>
 		</>
 	);
 }
