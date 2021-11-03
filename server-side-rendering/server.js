c6a16947526a0e3c4a0f27b3c1214c818722b572
Denis Mazurchuk <denis.aurorim@gmail.com>
2021-11-03 19:42:17 +0200

    Data loading with SSR

--- server-side-rendering/server.js
+++ server-side-rendering/server.js
@@ -7,6 +7,9 @@ import path from "path";
 import fs from "fs";
 import App from "./src/App";
 
+// to fix error indow is not exist on server
+global.window = {};
+
 const app = express();
 
 app.use(express.static("./build", { index: false }));
@@ -26,6 +29,7 @@ app.get("/*", (req, res) => {
   const sheet = new ServerStyleSheet();
 
   const reactApp = renderToString(
+    // add styles
     sheet.collectStyles(
       <StaticRouter location={req.url}>
         <App />
@@ -39,9 +43,19 @@ app.get("/*", (req, res) => {
     if (err) {
       return res.status(500).send(err);
     }
+
+    const loadedArticles = articles;
+
     return res.send(
       data
-        .replace('<div id="root"></div>', `<div id="root">${reactApp}</div>`)
+        .replace(
+          '<div id="root"></div>',
+          // to be able get data with SSR insert data into window object
+          `<script>window.preloadedArticles =
+            ${JSON.stringify(
+              loadedArticles
+            )}</script>  <div id="root">${reactApp}</div>`
+        )
         .replace(`{{ styles }}`, sheet.getStyleTags)
     );
   });
--- server-side-rendering/src/pages/Articles.js
+++ server-side-rendering/src/pages/Articles.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 
 export const Articles = () => {
-  const [articles, setArticles] = useState();
+  const [articles, setArticles] = useState(window && window.preloadedArticles);
 
   useEffect(() => {
-    fetch("/api/articles")
-      .then((response) => response.json())
-      .then((data) => setArticles(data));
+    if (window && !window.preloadedArticles) {
+      console.log("No preloaded articles found, loading from server");
+      fetch("/api/articles")
+        .then((response) => response.json())
+        .then((data) => setArticles(data));
+    }
   }, []);
 
   return (
